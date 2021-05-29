dd5d5a167ef50813d0eb498c974e844ea0352f0f
RachelChen <iceage91@gmail.com>
2021-05-29 15:35:53 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Restaurant List
+A simple web application for collecting favorite restaurant
+
+## Prerequisites
+Make sure you have installed the following prerequisites:
+- Node.js
+- Dependencies - Make sure you've installed Node.js and npm first, then install depencies using npm:
+$ npm install
+
+## Initializing project
+Make sure you've got all prerequisites, then initializing project by node using npm scripts:
+$ npm run start
+or initializing project by nodemon using:
+$ npm run dev
+
+## Features
+### Main page
+- Listing 8 favorite rastaurants
+- You can check out information of all restaurants which contains:
+	- image
+	- name
+	- category
+	- rating
+- Clicking each restaurant brings you to "Show page"
+
+### Show page
+- You can check more details about the restaurant. The details include:
+	- category
+	- address
+	- phone number
+	- descriptions
+	- image
+
+### Searching
+- type name or category of restaurant in search bar
+
+### Specifications
+- partil view
+- search input validation and feedback
---
+++ app.js
@@ -0,0 +1,54 @@
+const exphbs = require('express-handlebars')
+const express = require('express')
+const restaurants = require('./restaurant.json')
+const app = express()
+const port = 3000
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
+// static files
+app.use(express.static('public'))
+
+// routes settings
+app.get('/restaurants', (req, res) => {
+  if (!app.locals.partials) {
+    app.locals.partials = {};
+  }
+  // this will disable search template
+  app.locals.partials.isOnSearched = false
+  app.locals.partials.restaurantList = restaurants.results;
+  res.render(`index`, { style: 'main.css' })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurants.results.find(item => item.id.toString() === req.params.restaurant_id)
+  res.render(`show`, { restaurant, style: 'show.css' })
+})
+
+app.get('/search', (req, res) => {
+  if (!app.locals.partials) {
+    app.locals.partials = {};
+  }
+  // this will trigger search template to show search related information on page
+  app.locals.partials.isOnSearched = true
+  // show search information - remind user enter something and return function, no view action done
+  if (!req.query.keyword.trim().length) {
+    app.locals.partials.isSearchInputValid = false
+    res.render(`index`, { style: 'main.css' })
+    return
+  }
+  const filteredRestaurants = restaurants.results.filter(restaurant => {
+    // search by name and category, non case sensitive
+    return restaurant.name.trim().toLowerCase().includes(req.query.keyword.trim().toLowerCase()) || restaurant.category.trim().toLowerCase().includes(req.query.keyword.trim().toLowerCase())
+  })
+  // show search information - number of search resutls, view action - rendering filtered restaurants
+  app.locals.partials.isSearchInputValid = true
+  app.locals.partials.restaurantList = filteredRestaurants;
+  app.locals.partials.numberOfListedRestaurants = filteredRestaurants.length;
+  res.render(`index`, { keyword: req.query.keyword, style: 'main.css' })
+})
+
+app.listen(port, () => {
+  console.log(`app.js running on the https:\\localhost:${port}`)
+})
